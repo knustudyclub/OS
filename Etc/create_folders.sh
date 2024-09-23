4977fa8d6e756ae5b8fc5af9232098d87e23c06c
WithJo <researcherDragon@gmail.com>
2024-09-23 17:12:17 +0900

    chore: Add folders

--- Etc/create_folders.sh
+++ Etc/create_folders.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# 상위 디렉토리로 이동한 후 Week 01 ~ Week 12 폴더 생성 및 각 내부에 김준형, 윤정훈, 조재용 폴더 생성
+# Week 01 ~ Week 12 폴더 생성 및 각 내부에 김준형, 윤정훈, 조재용 폴더 생성
 # 그리고 각 주차 폴더에 README.md 파일을 생성
+# 각 이름 폴더에 ../Template/Questions Template.md 및 ../Template/Summery Template.md 복사
 
 for i in $(seq -w 1 12); do
-  # 상위 디렉토리로 이동하여 주차 폴더 생성
+  # 주차 폴더 생성
   mkdir -p "../Week $i/김준형" "../Week $i/윤정훈" "../Week $i/조재용"
 
-  # README.md 파일 생성 및 "## Week 숫자" 내용 추가
-  echo "## Week $i" > "../Week $i/README.md"
+  # 템플릿 파일을 각 이름 폴더로 복사
+  cp ../Template/Questions_Template.md "../Week $i/김준형/"
+  cp ../Template/Summery_Template.md "../Week $i/김준형/"
+
+  cp ../Template/Questions_Template.md "../Week $i/윤정훈/"
+  cp ../Template/Summery_Template.md "../Week $i/윤정훈/"
+
+  cp ../Template/Questions_Template.md "../Week $i/조재용/"
+  cp ../Template/Summery_Template.md "../Week $i/조재용/"
 done
 
 
---
+++ Week 02/조재용/Summery_Template.md
@@ -0,0 +1,19 @@
+## Summary 🧚
+
+`💡 {스터디에서 배운거 한줄요약}`
+
+## Concept 🧐
+
+{공부한 개념과 간단한 설명}
+
+## Key Takeaways 🔑
+
+{스터디에서 가장 중요한 개념을 작성}
+
+## Challenges & Difficulties 🤯
+
+{이해하기 어려웠던 부분이나 논의된 문제들}
+
+## References 📚
+
+{참고 자료나 추가적으로 공부한 자료}
